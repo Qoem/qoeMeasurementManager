1d790ff151de6b418f4b2cd8509bae8df35eb6c2
vagrant <vagrant@opencache-vagrant>
2015-02-19 11:55:32 +0000

    add setup req.

---
+++ install_requirements.sh
@@ -0,0 +1,17 @@
+  #!/bin/bash
+  pip install virtualenv
+  sudo pip install virtualenv
+  sudo pip install virtualenvwrapper
+  sudo apt-get install java6-openjdk-jre
+  sudo apt-get install openjdk6-jre
+  sudo apt-get install openjdk-6-jre
+  sudo apt-get install monodb
+  sudo apt-get install mongodb
+  cat .ssh/authorized_keys 
+  git clone https://github.com/Qoem/qoem
+  sudo apt-get install gcc 
+  sudo apt-get install python-lxml
+  sudo pip install aniso8601
+  sudo pip install requests
+  sudo pip install pymediainfo
+  sudo pip install flask
